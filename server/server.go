6e264df572171b2f41ff36819856330ef893ebe5
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2020-11-02 02:15:22 +0330

    Add file server handler for frontend

--- server/server.go
+++ server/server.go
@@ -15,6 +15,9 @@ func Run() {
 	router.HandleFunc("/", controllers.Index)
 	router.HandleFunc("/upload", controllers.Upload)
 
+	fs := http.FileServer(http.Dir("./frontend/dist/"))
+	router.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", fs))
+
 	fmt.Println("server started at: http://127.0.0.1:1313")
 	log.Fatalln(http.ListenAndServe(":1313", router))
 }
