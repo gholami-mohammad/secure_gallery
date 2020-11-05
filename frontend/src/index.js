8237c2c98604d63e333f9dc93bd97d33f4552dfa
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2020-11-06 02:47:32 +0330

    Display folders with style Display files Show image on click

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,2 +1,39 @@
-import 'jquery/dist/jquery';
-import 'bootstrap/dist/js/bootstrap';
+"use strict";
+
+window.$ = window.jQuery = require('jquery');
+require ('bootstrap');
+
+window.showImage = (filename) => {
+    var img = document.createElement('img');
+    img.id = "display-img"
+    img.src = "/getfile?filename=" + filename;
+    $('#img-wrapper').fadeIn(200);
+
+    const imgHolder = $('#img-wrapper #img-holder');
+    imgHolder.html(img);
+
+}
+
+window.closeImage = () => {
+    $('#img-wrapper').hide();
+}
+
+$(document).on('keyup',(e) => {
+    if (e.key === "Escape") {
+        closeImage();
+   }
+});
+
+$(document).on('click', (event) => { 
+    var target = $(event.target)[0];
+    if(target.id === 'display-img') {
+        return;
+    }
+    
+    switch(target.id) {
+        case 'display-img': return;
+        case 'img-wrapper': closeImage();
+        case 'img-holder': closeImage();
+        default: return;
+    }
+});
